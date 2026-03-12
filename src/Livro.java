a06420a425c07d04a3157c6f1e77fcc3a09796fc
Brenoell <brenobell03@gmail.com>
2026-03-12 09:45:27 -0300

    classe livro - criação de classe teste

---
+++ src/Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    //Atributos: Características do objeto
+    //Tipo nomeAtributo
+    String titulo;
+    String autor;
+    double preco;
+    String editora;
+    int paginas;
+    String resumo;
+    String tipoCapa;
+}
--- src/Media.java
+++ src/Media.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Média {
+public class Media {
     public static void main(String[] args) {
         double n1, n2;
         double result;
